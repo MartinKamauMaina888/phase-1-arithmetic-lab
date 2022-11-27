ded4858ddf0891a6154c211493de91645c219b19
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-11-27 13:05:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
 // Write your code here
+//multiplication
+let multiply = num1*num2;
+let num1=31;
+let num2=2
+let multiply1= 31*2;
+
+multiply1();
+
+//random
+let random = Math.random();
+let random1 = Math.floor (Math.random () *10 ) +1;
+
+random1();
+
+//modulus
+let mod = num3%num4;
+let num3 = 28;
+let num4 = 6;
+let mod1= 28%6;
+
+mod1();
+
+//maximum
+let max = Math.max();
+let max1 = (16,17,18,19,20);
+
+max1();
+
+
+  
